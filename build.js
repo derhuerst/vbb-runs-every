4dfb0d06e3f55e98c6b213c8ffad5a7fd0c242d5
Jannis R <mail@jannisr.de>
2016-07-29 16:53:15 +0200

    use fs for build temporarily

--- build.js
+++ build.js
@@ -1,39 +1,30 @@
 'use strict'
 
-const redis   = require('redis')
-const data    = require('vbb-trips')
+const pipe = require('multipipe')
+const data = require('vbb-trips')
 const through = require('through2')
+const ndjson = require('ndjson')
+const fs = require('fs')
 
 
 
-const onError = (err) => {
-	if (!err) return
-	console.error(err.stack || err.message)
-}
-
-const db = redis.createClient()
-db.on('error', onError)
-
-
-
-data.routes('all')
-.pipe(through.obj((route, _, cb) => {
-
-	const set = db.multi()
-	for (let i = 0; i < (route.stops.length - 1); i++) {
-		const stop = route.stops[i]
-		const next = route.stops[i + 1]
-		for (let j = 0; j < route.when.length; j++) {
-			const timestamp = route.when[j]
-			set.sadd(stop.s + '-' + next.s, (stop.t + timestamp) / 1000)
+pipe(
+	  data.routes('all')
+	, through.obj(function (route, _, cb) {
+		for (let i = 0; i < (route.stops.length - 1); i++) {
+			const stop = route.stops[i]
+			const next = route.stops[i + 1]
+			for (let j = 0; j < route.when.length; j++) {
+				const timestamp = route.when[j]
+				this.push([stop.s, next.s, (stop.t + timestamp) / 1000])
+			}
 		}
+		cb()
+	})
+	, ndjson.stringify()
+	, fs.createWriteStream('data.ndjson')
+	, (err) => {
+		if (!err) return console.info('done')
+		console.error('err', err.stack || err.message)
 	}
-
-	set.exec((err) => {if (err) return onError(err);cb()})
-}))
-
-.on('end', () => db.save((err) => {
-	if (err) onError(err)
-	db.quit(onError)
-	console.log('Done.')
-}))
+)
