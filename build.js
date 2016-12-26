6668de7f7fe84ec7d3d326e4c49b2fd24f9a0b28
Jannis R <mail@jannisr.de>
2016-12-27 00:28:00 +0100

    build in chunks

---
+++ build.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const process = require('child_process')
+const queue = require('queue')
+const os = require('os')
+
+const convert = require.resolve('./convert')
+
+const src = require.resolve('vbb-trips/data/routes.ndjson')
+const total = parseInt(process.execSync(`cat '${src}' | wc -l | bc`).toString())
+const chunk = 1000
+
+
+
+const q = queue({concurrency: os.cpus().length})
+
+q.on('error', (err) => {
+	console.error(err)
+	process.exit(1)
+})
+
+q.on('success', (_, job) => {
+	console.log('chunk', job.nr, 'finished')
+})
+
+
+
+const add = (from, to) => {
+	const job = (cb) => {
+		process.execFile('node', [convert, '' + from, '' + to], (err) => {
+			if (err) cb(err)
+			else cb()
+		})
+	}
+	job.nr = from / chunk
+	q.push(job)
+}
+
+for (let offset = 0; offset < total; offset += chunk) {
+	add(offset, Math.min(offset + chunk, total))
+}
+
+q.start()
